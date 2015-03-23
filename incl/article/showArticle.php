5c56583faf4ecfe91fe0ba8b0258cb6f2df926ed
John Doe <Bent_9_3@hotmail.com>
2015-03-23 13:37:29 +0100

    Show article
    Bundit samman article med object dynamiskt.
    Använder mig utav $_get för att veta vilken article jag är i. Upptaterat
    databasen för att länka samman. Sen när bilden ska visas så har jag lagt
    till php script för att ändra sökvägen så man kommer till en nerskalad
    bild direkt istället. Tänkte lägga in manuellt men skulle bli över 100
    st inskrivningar i databasen. Nu är det ochså dynamiskt. Klickar man på
    bilden kommer man till en helsida med biden.
    
    Att lägga in en php parameter i PDO var inte det lättaste. Var tvungen
    att lägga till en ny rad, bindParam

--- incl/article/showArticle.php
+++ incl/article/showArticle.php
@@ -10,63 +10,104 @@ $stmt = $db->prepare('SELECT * FROM Article WHERE category = "article";');
 $stmt->execute();
 $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $current = null;
-
-$select ="<select id='input1' name='article' onchange='form.submit();'>";
-$select .= "<option value='-1'>Välj Artikel</option>";
+$i= 0;
+//$select ="<select id='input1' name='article' onchange='form.submit();'>";
+//$select .= "<option value='-1'>Välj Artikel</option>";
 
 //checking the url for a query string	
+//Just the page
 $p=null;
 if(isset($_GET["p"])){
 	$p = $_GET["p"];
 }
-	
-foreach($res as $article){
-	$selected="";
-	if(isset($_POST['article']) && $_POST['article'] == $article['id']){
-		$selected = "selected";
-		$current = $article;
-	}
-	$select .="<option value='{$article['id']}' {$selected}>{$article['title']} ({$article['id']})</option>";
-	$articleTitle = $selected>$article['title'];
-	
-	}
-$articleTitletest = "<a href="?p=kmom03_get&art=Test"></a>";
-$select .= "</select>";
+//The article name
+$a=null;
+if(isset($_GET["a"])){
+	$a = $_GET["a"];
+}
 ?>
+	<!--Bör flyttas till sidobaren-->
+	<fieldset>
+		<ul>
+		<?php foreach($res as $article): ?>
+			<li><a href="<?php echo $currentArticle="?p=showArticle&a=" . $article['title']; ?>"><?php echo $article['title']; ?></a>
+		<?php endforeach; ?>
+		</ul>
+	</fieldset>
+	
+	<!--Plockar fram artikeln som användaren valt -->
+	<?php if(isset($a)){
+		foreach($res as $article){
+			if($a == $article['title']){
+				$current = $article;
+				//break;
+			}
+			$idArray[$i] = array($article['id']);
+			$i++;
+		}
+	}
+	?>
+	
+	<!--Presenterar valda artiklen -->
+	<?php if(isset($current)): ?>
 
-<form method="post">
+	<?php
+	//Add objetc
+	//
+	//$stmt = $db->prepare('SELECT * FROM Object WHERE articleLink = "Begravningskonfekt";');
+	$stmt = $db->prepare('SELECT * FROM Object WHERE articleLink = :parameter');
+	$stmt->bindParam(':parameter', $a, PDO::PARAM_STR);
+	$stmt->execute();
+	$oRes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	?>
+	
 	<fieldset>
 		<p>
-			<label for="input1">Artikel:</label><br>
-			<?php
-			foreach($res as $article){
-	
-				echo $article['title'];
-			}
-			
-			?>
-		</p>	
-	
-		<?php if(isset($current)): ?>
-			<p>
-				<div style="background:#eee; border:1px solid #eee;padding:1em;overflow:auto;">
-					<h1><?php echo $current['title']; ?></h1>
-					<p class="by-author"><?php echo $current['author'] . ', publicerad ' . $current['pubdate']; ?></p>
-					<p><?php echo $current['content']; ?></p>
-				</div>
-			</p>
-			
-			<p>	
-				<input type="submit" Name="Next" value="Next">
-				<?php
-				if(isset($_POST['Next'])){
-					//echo $next .= "{$article['id']}";
-					$colcount = $stmt->columnCount();
-					print($colcount);
+			<div style="background:#eee; border:1px solid #eee;padding:1em;overflow:auto;">
+				<h1><?php echo $current['title']; ?></h1>
+				<p class="by-author"><?php echo $current['author'] . ', publicerad ' . $current['pubdate']; ?></p>
+				<p><?php echo $current['content']; ?></p>
+				
+				<p>
+				<?php foreach($oRes as $images):?>
+					<!--Change search route-->
+					<!--Also makes image clickable for full size-->
+					<?php
+						$newPathImage['image'] = $images['image'];
+						$resizeImage ="bmo/250";
+						$newPathImage['image'] = str_replace("bmo", $resizeImage, $images['image']);
+					?>
+				<a href="<?php echo $images['image']?>"><img src="<?php echo $newPathImage['image']; ?>"></a>
+				
+				
+				<?php endforeach; ?>
+				</p>
+				
+				 <?php
+					// $nextarticle = $current['id'] + 1;
 					
-				}
-				?>				
-			<p>
-		<?php endif;?>
+					// for($i=0; i<count($idArray); $i++;){
+						// if($nextarticle == $value){
+							// echo $nextarticle;
+						// }
+					
+					// }
+					
+					// foreach ($idArray as $value) {
+						// if($nextarticle == $value){
+							// echo $nextarticle;
+						// }
+						// else{
+							// $nextarticle++;
+						// }
+					// }
+					//echo $nextarticle = $current['id'] + 1;
+					//print_r($idArray);
+			
+				// ?>
+				
+			</div>
+		</p>
 	</fieldset>
-</form>
+	
+	<?php endif;?>
